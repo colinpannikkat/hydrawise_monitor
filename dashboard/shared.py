f59c0eb84c2ccb40774543af851741e4c8f0be5e
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-07-04 00:28:35 -0700

    Enable global scoping for authentication persistence (possibly not a good idea, may change later)

--- dashboard/shared.py
+++ dashboard/shared.py
@@ -3,12 +3,15 @@ from src.monitor import Monitor
 import logging
 
 
+m = Monitor()
+cid, usr, pwd = get_env_variables()
+
+
 def get_df():
 
-    m = Monitor()
-    cid, usr, pwd = get_env_variables()
+    if not m.auth:
+        m.authenticate(usr, pwd)
 
-    m.authenticate(usr, pwd)
     df = m.get_flow_data_in_time_range(cid)
     df = m.find_outliers(df)
 
