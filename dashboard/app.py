fa467ac9a16ae7898549de1b4aabc2f0cfbb5f4e
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-07-04 19:05:14 -0700

    Add printed date range, limits on date selection, and total runtime for irrigation. Also add selection of zones for total.

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,15 +1,19 @@
 from shared import df, get_df
 from src.helpers import (
     get_one_month_back_in_datetime,
-    filter_dataframe_by_date
+    filter_dataframe_by_date,
+    filter_dataframe_by_zone,
+    get_data_range_available,
+    get_date_range_str_tuple
 )
-from datetime import datetime
 
 from shiny import App, render, reactive, ui, run_app
 import matplotlib.pyplot as plt
 import pandas as pd
 import logging
+import webbrowser
 
+PORT = 5253
 
 zone_group = (
     df[['zone', 'zone_num']]
@@ -18,11 +22,13 @@ zone_group = (
 )
 
 zone_choices = {
-    row.zone_num: f"{row.zone_num}: {row.zone}"
+    int(row.zone_num): f"{row.zone_num}: {row.zone}"
     for _, row in zone_group.iterrows()
 }
 
 title = "Hydrawise Irrigation Monitoring"
+
+min_date, max_date = get_date_range_str_tuple(df)
 
 app_ui = ui.page_fluid(
     ui.panel_title(title, title),
@@ -36,16 +42,33 @@ app_ui = ui.page_fluid(
             ui.input_switch("outliers", "Toggle Outliers", value=True),
             ui.input_date_range(
                 "date_range",
-                "Date Range",
+                "Date Range (Date selected must be within start and end range)",
                 start=get_one_month_back_in_datetime(),
-                end=datetime.today()),
+                end=max_date,
+                min=min_date,
+                max=max_date
+            ),
+            ui.output_text_verbatim("date_range_text_output"),
             ui.input_action_button("refresh", "Refresh Data"),
             width=300,
             style="padding: 20px;"
         ),
         ui.output_plot("irrigation_for_zone"),
-        ui.output_plot("total_irrigation_over_time"),
         style="padding: 30px;"
+    ),
+    ui.layout_sidebar(
+        ui.sidebar(
+            ui.input_checkbox_group(
+                "zone_select",
+                "",
+                choices=zone_choices,
+                selected=zone_group['zone_num'].unique()
+            ),
+            width=300,
+            style="padding: 20px;"
+        ),
+        ui.output_plot("total_irrigation_over_time"),
+        ui.output_plot("total_runtime_over_time")
     ),
     ui.tags.style(
         """
@@ -82,7 +105,7 @@ def server(input, output, session):
         ax.set_title(f"{zone_name} Irrigation", fontsize=14, fontweight='bold')
         ax.set_xlabel('Datetime', fontsize=12)
         ax.set_ylabel('GPM', fontsize=12)
-        ax.tick_params(axis='x', rotation=45)
+        ax.tick_params(axis='x', rotation=90)
         ax.grid(True, linestyle='--', alpha=0.7)
         plt.tight_layout()
         return fig
@@ -94,21 +117,53 @@ def server(input, output, session):
         group = df.copy()
         group = filter_dataframe_by_date(group, *input.date_range())
 
+        zone_list = [int(zone) for zone in input.zone_select()]
+        group = filter_dataframe_by_zone(group, zone_list)
+
         group['date'] = pd.to_datetime(group['datetime']).dt.date
-        agg = group.groupby(['date'])['gpm'].sum().reset_index()
+        agg = group.groupby(['date'])['gallons'].sum().reset_index()
 
         fig, ax = plt.subplots(figsize=(10, 5))
         ax.plot(
             agg['date'],
-            agg['gpm'],
+            agg['gallons'],
             marker='o',
             linestyle='-',
         )
 
-        ax.set_title("Total Irrigation over Time", fontsize=14, fontweight='bold')
+        ax.set_title("Total Water Usage over Time", fontsize=14, fontweight='bold')
         ax.set_xlabel('Datetime', fontsize=12)
-        ax.set_ylabel('Total GPM', fontsize=12)
-        ax.tick_params(axis='x', rotation=45)
+        ax.set_ylabel('Total Gallons', fontsize=12)
+        ax.tick_params(axis='x', rotation=90)
+        ax.grid(True, linestyle='--', alpha=0.7)
+        plt.tight_layout()
+        return fig
+
+    @reactive.calc
+    @render.plot
+    def total_runtime_over_time():
+        trigger.get()
+        group = df.copy()
+        group = filter_dataframe_by_date(group, *input.date_range())
+
+        zone_list = [int(zone) for zone in input.zone_select()]
+        group = filter_dataframe_by_zone(group, zone_list)
+
+        group['date'] = pd.to_datetime(group['datetime']).dt.date
+        agg = group.groupby(['date'])['runtime'].sum().reset_index()
+
+        fig, ax = plt.subplots(figsize=(10, 5))
+        ax.plot(
+            agg['date'],
+            agg['runtime'],
+            marker='o',
+            linestyle='-',
+        )
+
+        ax.set_title("Total Runtime over Time", fontsize=14, fontweight='bold')
+        ax.set_xlabel('Datetime', fontsize=12)
+        ax.set_ylabel('Total Runtime', fontsize=12)
+        ax.tick_params(axis='x', rotation=90)
         ax.grid(True, linestyle='--', alpha=0.7)
         plt.tight_layout()
         return fig
@@ -123,6 +178,11 @@ def server(input, output, session):
 
         trigger.set(trigger.get() + 1)
 
+    @render.text
+    def date_range_text_output():
+        return get_data_range_available(df)
+
 
 app = App(app_ui, server)
-run_app(app, port=0)
+webbrowser.open(f"http://127.0.0.1:{PORT}")
+run_app(app, port=PORT)
